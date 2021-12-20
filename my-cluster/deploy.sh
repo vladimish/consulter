f4f51d977656647a92955c9f06d8f194317e4cf9
Vladimir Mishakov <01.vladimir.mishakov@gmail.com>
2021-12-20 06:25:24 +0300

    Add new k8s configs.

--- my-cluster/deploy.sh
+++ my-cluster/deploy.sh
@@ -3,7 +3,14 @@ kubectl apply -f mysql-auth-config.yaml
 kubectl apply -f rabbitmq-config.yaml
 kubectl apply -f mysql-auth-secret.yaml
 kubectl apply -f rabbitmq-secret.yaml
+kubectl apply -f gateway-config.yaml
+kubectl apply -f mysql-timetable-secret.yaml
+kubectl apply -f mysql-timetable-config.yaml
+
+kubectl apply -f mysql-timetable.yaml
 kubectl apply -f rabbitmq.yaml
 kubectl apply -f mysql-auth.yaml
 kubectl apply -f gateway.yaml
 kubectl apply -f auth.yaml
+kubectl apply -f frontend.yaml
+kubectl apply -f timetable.yaml
