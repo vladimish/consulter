65ee3667d0469e777f7f3f33c9ce1a98cc81350e
Vladimir Mishakov <01.vladimir.mishakov@gmail.com>
2021-12-20 08:55:21 +0300

    Test fix.

--- frontend/src/main/java/com/vladimish/consulter/frontend/EnvConfig.java
+++ frontend/src/main/java/com/vladimish/consulter/frontend/EnvConfig.java
@@ -7,7 +7,7 @@ public class EnvConfig {
     public static String gatewayURL;
 
     public static void Load(){
-        gatewayURL = System.getenv("GATEWAY_SERVICE_PORT");
+        gatewayURL = "http://" + System.getenv("GATEWAY_SERVICE_SERVICE_HOST") + ":" + System.getenv("GATEWAY_SERVICE_SERVICE_PORT");
         if (gatewayURL == null){
             log.error("Variable GATEWAY_SERVICE_PORT isn't set.");
         }
